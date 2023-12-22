fc9bb7dd624c4719ae8fc0b9d5a19f0ee62ed460
YMisterY <joaopedromgil2@gmail.com>
2023-12-22 01:31:27 +0000

    v3

--- app/src/main/java/com/example/ucityventure/CustomAdapter.java
+++ app/src/main/java/com/example/ucityventure/CustomAdapter.java
@@ -1,8 +1,11 @@
 package com.example.ucityventure;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
@@ -20,6 +30,7 @@ import java.util.concurrent.Executors;
 public class CustomAdapter extends ArrayAdapter<Ride> {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String uuid = FirebaseAuth.getInstance().getCurrentUser().getUid().toString();
     Executor executor = Executors.newSingleThreadExecutor();
     Handler mainHandler = new Handler(Looper.getMainLooper());
 
@@ -54,6 +65,8 @@ public class CustomAdapter extends ArrayAdapter<Ride> {
         rideDesc.setText("Desde " + ride.getOrigin());
         rideCount.setText(ride.getRidePassangers().size() + "/" + ride.getRideCapacity());
 
+
+        //Parte visual
         if(ride.getRidePassangers().size() >= ride.getRideCapacity()){
             //cheia
             rideButton.setEnabled(false);
@@ -65,13 +78,42 @@ public class CustomAdapter extends ArrayAdapter<Ride> {
             rideButton.setText("Entrar");
         }
 
+        if(ride.getRidePassangers().contains(uuid)){
+            //o user ta inscrito nesta ride
+            rideButton.setEnabled(true);
+            rideButton.setText("Inscrito");
+        } else {
+            //nao esta inscrito
+        }
 
+        //ordem:
+        //ta inscrito?
+        //
+
+        View finalConvertView = convertView;
         rideButton.setOnClickListener(v -> {
-            if(ride.getRidePassangers().size() < ride.getRideCapacity()){
-                //tem vagas
+            if(ride.getRidePassangers().contains(uuid)){
+                //ta inscrito - Sair
+                unsubRide(uuid, ride);
+                rideButton.setText("Entrar");
 
             } else {
-                //nao tem vagas
+                //nao ta
+                if(rideButton.getText().toString().equals("Entrar")){
+                    //quer entrar
+                    if(ride.getRidePassangers().size() < ride.getRideCapacity()){
+                        //ha vagas
+                        subRide(uuid, ride);
+                        rideButton.setText("Sair");
+                    } else {
+                        //nao ha vagas
+                        Snackbar.make(finalConvertView, "Não existem vagas", Snackbar.LENGTH_SHORT).show();
+                    }
+                } else {
+                    //quer sair
+                    unsubRide(uuid, ride);
+                    rideButton.setText("Entrar");
+                }
             }
         });
 
@@ -86,6 +128,65 @@ public class CustomAdapter extends ArrayAdapter<Ride> {
 
         // Return the completed view to render on screen
         return convertView;
+    }
+
+
+
+    public void subRide(String userID, Ride ride){
+
+        ArrayList<String> novaLista = ride.getRidePassangers();
+        novaLista.add(userID);
+
+
+        executor.execute(() -> {
+            DocumentReference docRef = db.collection("rides").document(ride.getId());
+
+            docRef.update("ridePassangers", novaLista)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            mainHandler.post(() -> {
+                                Log.d(TAG, "Note updated successfully!");
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            mainHandler.post(() -> {
+                                Log.w(TAG, "Error updating note", e);
+                            });
+                        }
+                    });
+        });
+    }
+
+    public void unsubRide(String userID, Ride ride){
+        ArrayList<String> novaLista = ride.getRidePassangers();
+        novaLista.remove(userID);
+
+
+        executor.execute(() -> {
+            DocumentReference docRef = db.collection("rides").document(ride.getId());
+
+            docRef.update("ridePassangers", novaLista)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            mainHandler.post(() -> {
+                                Log.d(TAG, "Note updated successfully!");
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            mainHandler.post(() -> {
+                                Log.w(TAG, "Error updating note", e);
+                            });
+                        }
+                    });
+        });
     }
 
 }
