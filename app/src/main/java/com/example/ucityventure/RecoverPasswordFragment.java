97db574461269d401f92f2e2db3d7cdd2ca0d896
YMisterY <joaopedromgil2@gmail.com>
2024-01-13 17:36:15 +0000

    v3

--- app/src/main/java/com/example/ucityventure/RecoverPasswordFragment.java
+++ app/src/main/java/com/example/ucityventure/RecoverPasswordFragment.java
@@ -99,6 +99,8 @@ public class RecoverPasswordFragment extends Fragment {
                                 }
                             }
                         });
+
+                ((MainActivity)getActivity()).PopCurrentFragment();
             }
         });
 
